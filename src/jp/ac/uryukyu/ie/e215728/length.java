258226631d376a0cf5dc14c65421ea21f47cae5f
Kouki Hoshi <e215728@ie.u-ryukyu.ac.jp>
2022-02-08 21:34:41 +0900

    1st commit

---
+++ src/jp/ac/uryukyu/ie/e215728/length.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e215728;
+
+public class length {
+    public static void main (String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
