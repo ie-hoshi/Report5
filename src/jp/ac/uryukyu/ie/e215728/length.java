c23256f908386355b05d6afcb93c27cddf4740cc
Kouki Hoshi <e215728@ie.u-ryukyu.ac.jp>
2022-02-08 23:04:06 +0900

    3rd commit

--- src/jp/ac/uryukyu/ie/e215728/length.java
+++ src/jp/ac/uryukyu/ie/e215728/length.java
@@ -2,12 +2,14 @@ package jp.ac.uryukyu.ie.e215728;
 
 public class length {
     public static void main (String[] args){
-        try{
+        /*try{
             String str = null;
             System.out.println(str.length());
         } catch (NullPointerException e){
             System.out.println("NullPointerExceptionが発生");
             System.out.println(e.getMessage());
-        }
+        }*/
+        String str = "百二十三";
+        Integer.parseInt(str);
     }
 }
